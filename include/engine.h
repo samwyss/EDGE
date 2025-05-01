f9a23d859877d9e18e7cfbbf474f5a24273723ab
sam <sjwyss@outlook.com>
2025-05-01 09:54:46 -0400

    began outlining engine api

--- include/engine.h
+++ include/engine.h
@@ -1,10 +1,25 @@
 #ifndef EDGE_ENGINE_H
 #define EDGE_ENGINE_H
 
-
-
+/*!
+ * a facade class for interacting with EDGE within simulations
+ */
 class Engine {
+public:
+  Engine();
 
+  ~Engine();
+
+  void advance_by();
+
+  void advance_to();
+
+  void set_hdf5_io();
+
+  void log();
+
+private:
+  void advance();
 };
 
 
